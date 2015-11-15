01e3985bab85aa258febbc96f11d61b18d4c220f
Barbara Geller <barbara@copperspice.com>
2015-11-14 18:33:24 -0800

    Renamed and updated test files

---
+++ test_build/dox/test_group.h
@@ -0,0 +1,39 @@
+/*************************************************************************
+*                                                                         
+* Copyright (C) 2012-2015 Barbara Geller & Ansel Sermersheim                                                         
+* All rights reserved.                                                    
+*                                                                         
+*                                                                         
+* GNU Free Documentation License                                          
+* This file may be used under the terms of the GNU Free Documentation     
+* License version 1.3 as published by the Free Software Foundation        
+* and appearing in the file included in the packaging of this file.       
+*                                                                         
+*                                                                         
+*************************************************************************/
+
+/** This class contains a group of constructors */
+
+class groupTest 
+{
+   public:
+
+/** @name Group of Constructors
+ *  Type safe and safer than exposing a var arg constructor.
+ */
+
+/* @{ */
+
+  /** constructor a - this works */
+  groupTest();
+
+  groupTest(long);             /**< constructor b  - this works  */ 
+
+  groupTest(bool);             ///< constructor c - broken
+
+  groupTest(int);              //!< constructor d - broken 
+
+
+/* @} */
+
+};
