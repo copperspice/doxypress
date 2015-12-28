490cb74d1582e6cd57cc07e6e5ca39b8c0ddde9e
Barbara Geller <barbara@copperspice.com>
2015-12-27 23:50:14 -0800

    Copyright update

--- test_build/dox/test_ssl.h
+++ test_build/dox/test_ssl.h
@@ -1,6 +1,6 @@
 /*************************************************************************
 *                                                                         
-* Copyright (C) 2012-2015 Barbara Geller & Ansel Sermersheim                                                         
+* Copyright (C) 2012-2016 Barbara Geller & Ansel Sermersheim                                                         
 * All rights reserved.                                                    
 *                                                                         
 *                                                                         
