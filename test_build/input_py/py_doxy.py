d4cac29fc37656fe482208dc6175bbcfdb3e3932
Barbara Geller <barbara@copperspice.com>
2016-06-18 14:43:37 -0800

    Add python test suite

---
+++ test_build/input_py/py_doxy.py
@@ -0,0 +1,30 @@
+## @package py_doxy
+#  Documentation for package using doxy comment syntax.
+#
+#  More details (doxy-1).
+
+## Documentation for a function.
+#
+#  More details (doxy-2).
+def func():
+    pass
+
+## Documentation for a class (brief).
+#
+#  More details (doxy-3).
+class PyClass:
+   
+    ## The constructor documentation.
+    def __init__(self):
+        self._memVar = 0;
+   
+    ## Documentation for a method.
+    #  @param self The object pointer.
+    def PyMethod(self):
+        pass
+     
+    ## A class variable.
+    classVar = 0;
+
+    ## @var _memVar
+    #  This is the documentation for a member variable.
