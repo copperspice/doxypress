04a881ad265770d5a703be4ec316f476608f5423
Barbara Geller <barbara@copperspice.com>
2019-01-11 11:26:43 -0800

    Update copyright and license

--- src/outputlist.cpp
+++ src/outputlist.cpp
@@ -1,18 +1,19 @@
-/*************************************************************************
- *
- * Copyright (C) 2014-2018 Barbara Geller & Ansel Sermersheim
- * Copyright (C) 1997-2014 by Dimitri van Heesch.
- * All rights reserved.
- *
- * Permission to use, copy, modify, and distribute this software and its
- * documentation under the terms of the GNU General Public License version 2
- * is hereby granted. No representations are made about the suitability of
- * this software for any purpose. It is provided "as is" without express or
- * implied warranty. See the GNU General Public License for more details.
- *
- * Documents produced by DoxyPress are derivative works derived from the
- * input used in their production; they are not affected by this license.
- *
+/************************************************************************
+*
+* Copyright (C) 2014-2019 Barbara Geller & Ansel Sermersheim
+* Copyright (C) 1997-2014 by Dimitri van Heesch
+*
+* DoxyPress is free software: you can redistribute it and/or
+* modify it under the terms of the GNU General Public License version 2
+* as published by the Free Software Foundation.
+*
+* DoxyPress is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* Documents produced by DoxyPress are derivative works derived from the
+* input used in their production; they are not affected by this license.
+*
 *************************************************************************/
 
 #include <outputlist.h>
