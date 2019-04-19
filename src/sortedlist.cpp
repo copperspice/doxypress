405f2c036c09c230250f59573ec60791a76f7e8e
Barbara Geller <barbara@copperspice.com>
2019-04-19 10:41:31 -0800

    Add new test in compareListValues() for a Memberdef, fix line endings

--- src/sortedlist.cpp
+++ src/sortedlist.cpp
@@ -28,7 +28,7 @@
 #include <groupdef.h>
 
 // BaseClassList --> SortedList<BaseClassDef *>
-int compareListValues(const BaseClassDef *item1, const BaseClassDef *item2, bool flag) 
+int compareListValues(const BaseClassDef *item1, const BaseClassDef *item2, bool flag)
 {
    QSharedPointer<const ClassDef> c1 = item1->classDef;
    QSharedPointer<const ClassDef> c2 = item2->classDef;
@@ -47,7 +47,7 @@ int compareListValues(const QSharedPointer<ClassDef> item1, const QSharedPointer
 {
    static bool b = Config::getBool("sort-by-scope-name");
 
-   if (b) {   
+   if (b) {
       return item1->name().compare(item2->name(), Qt::CaseInsensitive);
 
    } else {
@@ -65,7 +65,7 @@ int compareListValues(const QSharedPointer<DirDef> &item1, const QSharedPointer<
 
 
 // DotNodeList --> SortedList<DotNode *>
-int compareListValues(const DotNode *item1, const DotNode *item2, bool flag) 
+int compareListValues(const DotNode *item1, const DotNode *item2, bool flag)
 {
    return item1->label().compare(item2->label(), Qt::CaseInsensitive);
 }
@@ -73,7 +73,7 @@ int compareListValues(const DotNode *item1, const DotNode *item2, bool flag)
 
 // FileNameList --> SortedList<FileDef *>   true
 // FileList     --> SortedList<FileDef *>   false
-int compareListValues(const QSharedPointer<FileDef> &item1, const QSharedPointer<FileDef> &item2, bool flag) 
+int compareListValues(const QSharedPointer<FileDef> &item1, const QSharedPointer<FileDef> &item2, bool flag)
 {
    if (flag) {
       return item1->fileName().compare(item2->fileName(), Qt::CaseInsensitive);
@@ -84,11 +84,11 @@ int compareListValues(const QSharedPointer<FileDef> &item1, const QSharedPointer
 
 
 // Doxy_Globals::inputNameList --> SortedList<FileNameList *>
-int compareListValues(const QSharedPointer<FileNameList> &item1, const QSharedPointer<FileNameList> &item2, bool flag) 
+int compareListValues(const QSharedPointer<FileNameList> &item1, const QSharedPointer<FileNameList> &item2, bool flag)
 {
    int retval;
 
-   if (Config::getBool("full-path-names")) {    
+   if (Config::getBool("full-path-names")) {
       retval = item1->fullName().compare(item2->fullName(), Qt::CaseInsensitive);
 
    } else {
@@ -101,7 +101,7 @@ int compareListValues(const QSharedPointer<FileNameList> &item1, const QSharedPo
 
 
 // GroupList --> SortedList<GroupDef *>
-int compareListValues(const QSharedPointer<GroupDef> &item1, const QSharedPointer<GroupDef> &item2, bool flag) 
+int compareListValues(const QSharedPointer<GroupDef> &item1, const QSharedPointer<GroupDef> &item2, bool flag)
 {
    return item1->groupTitle().compare(item2->groupTitle());
 }
@@ -124,8 +124,12 @@ int compareListValues(const QSharedPointer<MemberDef> &c1, const QSharedPointer<
 
       }
    }
-   
+
    int cmp = c1->name().compare(c2->name(), Qt::CaseInsensitive );
+
+   if (cmp == 0) {
+      cmp = c1->argsString().compare(c2->argsString(), Qt::CaseInsensitive );
+   }
 
    return cmp != 0 ? cmp : c1->getDefLine() - c2->getDefLine();
 }
@@ -133,7 +137,7 @@ int compareListValues(const QSharedPointer<MemberDef> &c1, const QSharedPointer<
 
 // NavIndexEntryList --> SortedList<NavIndexEntry *>
 int compareListValues(const NavIndexEntry *item1, const NavIndexEntry *item2, bool flag)
-{  
+{
    // sort list based on url
-   return item1->m_url.compare(item2->m_url);  
+   return item1->m_url.compare(item2->m_url);
 }
