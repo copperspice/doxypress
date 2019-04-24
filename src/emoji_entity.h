72b2ffe88bb4a0719aab55638d35feaddbf9da9c
Barbara Geller <barbara@copperspice.com>
2019-04-24 09:08:41 -0800

    Add two new files for emoji support

---
+++ src/emoji_entity.h
@@ -0,0 +1,45 @@
+/************************************************************************
+*
+* Copyright (C) 2014-2019 Barbara Geller & Ansel Sermersheim
+* Copyright (C) 1997-2014 by Dimitri van Heesch
+*
+* DoxyPress is free software: you can redistribute it and/or
+* modify it under the terms of the GNU General Public License version 2
+* as published by the Free Software Foundation.
+*
+* DoxyPress is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* Documents produced by DoxyPress are derivative works derived from the
+* input used in their production; they are not affected by this license.
+*
+*************************************************************************/
+
+#ifndef EMOJI_ENTITY_H
+#define EMOJI_ENTITY_H
+
+#include <QHash>
+#include <QFile>
+#include <QTextStream>
+
+class EmojiEntityMapper
+{
+  public:
+    static EmojiEntityMapper *instance();
+    static void deleteInstance();
+
+    QString name(int index) const;
+    QString unicode(int index) const;
+    void writeEmojiFile(QFile &file);
+    int symbol2index(const QString &symName) const;
+
+  private:
+    EmojiEntityMapper();
+   ~EmojiEntityMapper();
+
+    static EmojiEntityMapper *s_instance;
+    QHash<QString, int>       m_name2sym;
+};
+
+#endif
