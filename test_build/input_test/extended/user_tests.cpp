cc3466d185c6d741c2994c20d2a9f7c541a63099
Barbara Geller <barbara@copperspice.com>
2019-04-26 14:39:54 -0800

    Changes to the test suite, add new tests, change text on mainpage

--- test_build/input_test/extended/user_tests.cpp
+++ test_build/input_test/extended/user_tests.cpp
@@ -13,3 +13,47 @@ testTypeDef::myTypeDef testTypeDef::someMethod()
 
 
 
+/**
+Documenting class TagTest.
+
+\if MY_TAG
+This text should not appear in the output since MY_TAG would need to be true, and it is not set.  BAD
+\endif
+\ifnot MY_TAG
+This text will appear in the output since MY_TAG is false.  ( GOOD, this should show up in the output )
+\endif
+*/
+class TagTest
+{
+};
+
+
+
+/// Test overflow issue for Class_A through Class_E
+class Class_B : public Class_A
+{
+   public:
+      /** Documentation for method b1 */
+      void b1();
+};
+
+
+class Class_C : public Class_B
+{
+   public:
+};
+
+
+template <class T1, class T2>
+class Class_D : public Class_C
+{
+   public:
+};
+
+
+template <class T>
+class Class_E : public Class_D<T, T>
+{
+   public:
+};
+
